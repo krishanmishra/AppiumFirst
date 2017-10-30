933ea8685d22ac4256f66d352c1f58c326324b33
krishanm <kkmjssate@gmail.com>
2017-10-30 17:45:19 +0530

    Search inventory

--- src/test/java/com/ivm/android/scripts/SearchInventoryList.java
+++ src/test/java/com/ivm/android/scripts/SearchInventoryList.java
@@ -1,5 +1,58 @@
 package com.ivm.android.scripts;
 
-public class SearchInventoryList {
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.ivm.android.screenHelper.AppraisallistHelper;
+import com.ivm.android.screenHelper.InventoryListHelper;
+import com.ivm.android.screenHelper.UserLoginHelper;
+import com.ivm.android.util.DriverTestCase;
+
+public class SearchInventoryList extends DriverTestCase {
+	
+	
+	
+	// Initialize the objects
+	InventoryListHelper inventorylistHelper = new InventoryListHelper(getDriver());
+	UserLoginHelper userloginhelper=new UserLoginHelper(getDriver());
+	String searchvehicle="2014";
+	
+	
+	@Test(enabled = true)
+	public void searchbyVehicleInventory() throws InterruptedException
+	{
+		//user logged in successfully
+		login("ON");
+	
+		//Go to appraisal list screen
+		inventorylistHelper.inventoryList();
+		
+		//Tap on Magnification icon
+		inventorylistHelper.TapmagnifiICon();
+		
+		//Search vehicles with vehicle info name
+		inventorylistHelper.Searchresults(searchvehicle);
+		Reporter.log("search vehicle with VIN and result");
+				
+		//Tap on Back button 
+		inventorylistHelper.backAction();
+		Reporter.log("User tap on back action return to Home");
+		
+		// Tap on the Sign out button
+		userloginhelper.loggedOut();
+		Reporter.log("User taps on the Sign out button");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
