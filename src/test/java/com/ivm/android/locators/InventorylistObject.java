0113a77cdf25ab12c344efed0786e53ad3df45a8
krishanm <kkmjssate@gmail.com>
2017-10-30 17:42:51 +0530

    InventoryList

--- src/test/java/com/ivm/android/locators/InventorylistObject.java
+++ src/test/java/com/ivm/android/locators/InventorylistObject.java
@@ -7,5 +7,10 @@ public class InventorylistObject {
 	public final static String stock_number = "com.ivm.appraisal:id/stock_number";
 	public final static String mileage = "com.ivm.appraisal:id/mileage";
 	public final static String age = "com.ivm.appraisal:id/age";
-
+	public final static String inventorytitle = "com.ivm.appraisal:id/inventory_title";
+	public final static String vehicleInformation = "com.ivm.appraisal:id/vehicle_information";
+	public final static String imageView= "com.ivm.appraisal:id/image_view";
+	public final static String condition= "com.ivm.appraisal:id/condition";
+	public final static String state= "com.ivm.appraisal:id/state";
+	public final static String novehicle = "com.ivm.appraisal:id/com.ivm.appraisal:id/no_vehicles_inventory";
 }
